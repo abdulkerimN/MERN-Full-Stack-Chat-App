4a793235854daab31159986f84a62e3fd1004c23
abdu <abdualkerim01@gmail.com>
2025-11-19 15:10:19 +0300

    favicon message icon added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <h1>Abdu you can do anything and you will have good ending. just keep what ur doing and be good at it you will be the most wanted person have you seen the poster design you made , so just like that do lots of works in web, mobile, or any other tech stacks you will see how you become wanted developer, designer, just beka you will be betam important person zare yalutn chgroch bicha atyachew rash ly mesratkn ketl nege yeteshale yihonal inshallah. tsdt yalu srawochin sertek endemtchil asay,your real skill sewochin attract yadergal really.  </h1>
     </div>
   )
 }
