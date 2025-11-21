76ab45b26f5c87c6b4e4d67fe401aef7a1cb5f0d
abdu <abdualkerim01@gmail.com>
2025-11-21 21:20:14 +0300

    upgates made

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { Routes } from 'react-router-dom'
+import HomePage from './pages/Homepage.jsx'
 const App = () => {
   return (
     <div>
-      <h1>Abdu you can do anything and you will have good ending. just keep what ur doing and be good at it you will be the most wanted person have you seen the poster design you made , so just like that do lots of works in web, mobile, or any other tech stacks you will see how you become wanted developer, designer, just beka you will be betam important person zare yalutn chgroch bicha atyachew rash ly mesratkn ketl nege yeteshale yihonal inshallah. tsdt yalu srawochin sertek endemtchil asay,your real skill sewochin attract yadergal really.  </h1>
+      <Routes>
+        <Route  path='/' element="{<Homepage>}" />
+      </Routes>
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      
+      <h1>peace nw </h1>
     </div>
   )
 }
