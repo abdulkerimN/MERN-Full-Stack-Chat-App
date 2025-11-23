d9d068516e4acd0e5e02f3aa54b4f0cfb15a1b8f
abdu <abdualkerim01@gmail.com>
2025-11-23 23:44:36 +0300

    the App file modified

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route  path='/' element="{<Homepage>}" />
+        <Route  path='/' element="{<Homepage />}" />
       </Routes>
     </div>
   )
