df96a23fa8a36a6c3591c0cce6f958bffb82b648
abdu <abdualkerim01@gmail.com>
2025-12-05 23:51:40 +0300

    sidebar updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -43,8 +43,14 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
 
       <div className="flex flex-col">
         {userDummyData.map((user, index) => (
-          <div className={ `relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${
-  selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
+          <div
+            onClick={() => {
+              setSelectedUser(user);
+            }} key={index}
+            className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${
+              selectedUser?._id === user._id && "bg-[#282142]/50"
+            }`}
+          >
             <img
               src={user?.profilePic || assets.avatar_icon}
               alt=""
