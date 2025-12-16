b6b9d1c2f87d5d69881c46af6e03573afe166fa7
abdu <abdualkerim01@gmail.com>
2025-12-16 22:57:14 +0300

    chatcontainer updated

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import assets from '../assets/assets';
 const ChatContainer = ({selectedUser,setSelectedUser}) => {
   return (
-    <div>
+    <div className="h-full overflow-scroll relative backdrop-blur-lg">
       <div className="flex items-center gap-3 py-3 mx-4  border-b border-stone-500">
         <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
         <p className="flex-1 text-lg text-white flex items-center gap-2">
