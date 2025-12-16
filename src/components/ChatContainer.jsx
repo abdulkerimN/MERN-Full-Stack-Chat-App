c87945dd8ca539dfc309139e346d9615c00d8fcb
abdu <abdualkerim01@gmail.com>
2025-12-16 22:51:31 +0300

    chatcontainer updated

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -6,9 +6,15 @@ const ChatContainer = () => {
       <div className="flex items-center gap-3 py-3 mx-4  border-b border-stone-500">
         <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
         <p className="flex-1 text-lg text-white flex items-center gap-2">
-            Martin Johnson
+          Martin Johnson
           <span className="w-2 h-2 rounded-full bg-green-500"></span>
         </p>
+        <img
+          onClick={() => setSelectedUser(null)}
+          src={assets.arrow_icon}
+          alt=""
+          className="md:hidden max-w-7"
+        />
       </div>
     </div>
   );
